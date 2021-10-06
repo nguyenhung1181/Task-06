858914939b59fe2ae1f32f3f452c34923e3b8b97
nguyenmanhhung280901 <nguyenmanhhung280901@gmail.com>
2021-10-06 17:09:15 +0700

    Done

--- UserControlDataTemplate/MainPage.xaml.cs
+++ UserControlDataTemplate/MainPage.xaml.cs
@@ -12,28 +12,19 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using xBindDataExample.Models;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace xBindDataExample
+namespace UserControlDataTemplate
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private List<Book> Books;
         public MainPage()
         {
             this.InitializeComponent();
-            Books = BookManager.GetBooks();
-        }
-
-        private void GridView_ItemClick(object sender, ItemClickEventArgs e)
-        {
-            var book = (Book)e.ClickedItem;
-            ResultTextBlock.Text = "You selected " + book.Title;
         }
     }
 }
